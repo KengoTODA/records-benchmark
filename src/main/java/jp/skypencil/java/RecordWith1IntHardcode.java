6fb5d9dd78caca680cd32de3c6c43c4cbc964cca
Kengo TODA <skypencil@gmail.com>
2021-04-13 18:02:45 +0800

    avoid boxing

--- src/main/java/jp/skypencil/java/RecordWith1IntHardcode.java
+++ src/main/java/jp/skypencil/java/RecordWith1IntHardcode.java
@@ -7,12 +7,14 @@ record RecordWith1IntHardcode(int value) {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
+
         RecordWith1IntHardcode that = (RecordWith1IntHardcode) o;
+
         return value == that.value;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(value);
+        return value;
     }
 }
